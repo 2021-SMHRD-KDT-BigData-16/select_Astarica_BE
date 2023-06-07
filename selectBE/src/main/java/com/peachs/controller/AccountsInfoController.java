d28b3b6797e2fc09ca5edf93b856c7ae9079578e
webobae@naver.com <127809733+webobae@users.noreply.github.com>
2023-06-07 18:47:58 +0900

    json ok

--- selectBE/src/main/java/com/peachs/controller/AccountsInfoController.java
+++ selectBE/src/main/java/com/peachs/controller/AccountsInfoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.peachs.entity.AccountsInfo;
@@ -111,4 +112,5 @@ public class AccountsInfoController {
 			System.out.println(temp);
 			return "home";
 		}
+
 }
